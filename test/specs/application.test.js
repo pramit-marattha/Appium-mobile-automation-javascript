4c59836162a97fca7298e2072329c0923c41d9c2
pramit-marattha <pramitmarattha5497@gmail.com>
2021-04-24 01:34:13 +0545

    project setting up

---
+++ test/specs/application.test.js
@@ -0,0 +1,6 @@
+const LoginPage = require("../pageobjects/login.page");
+const SecurePage = require("../pageobjects/secure.page");
+
+describe("My application", () => {
+  it("Test", () => {});
+});
